b4006d60cc224b96d9235214bdeb807a3803335c
Mumuni Mohammed <mumunim10@gmail.com>
2021-09-01 00:17:22 +0000

    Day 1: One approach added.

--- solutions/day1/kalkulus.py
+++ solutions/day1/kalkulus.py
@@ -1,1 +1,13 @@
-# A test file
+# A test solution
+
+def swap_first_and_last(l):
+
+    first, *middle, last = l
+
+    l = [last, *middle, first]
+
+    return l
+
+print(swap_first_and_last([1, 2, 3]))
+print(swap_first_and_last([12, 35, 9, 56, 24]))
+
